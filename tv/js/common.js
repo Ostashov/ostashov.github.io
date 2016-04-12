b1d6baa1df9dfa696e4f784dcd837cde7e705c9c
Oleg Ostashov <oleg.ostashov@gmail.com>
2016-04-12 14:44:58 +0300

    common.js updated

--- tv/js/common.js
+++ tv/js/common.js
@@ -1,61 +1,5 @@
 $(document).ready(function() {
 
-    //Таймер обратного отсчета
-    //Документация: http://keith-wood.name/countdown.html
-    //<div class="countdown" date-time="2015-01-07"></div>
-    var austDay = new Date($(".countdown").attr("date-time"));
-    $(".countdown").countdown({until: austDay, format: 'yowdHMS'});
-
-    //Попап менеджер FancyBox
-    //Документация: http://fancybox.net/howto
-    //<a class="fancybox"><img src="image.jpg" /></a>
-    //<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-    $(".fancybox").fancybox();
-
-    //Навигация по Landing Page
-    //$(".top_mnu") - это верхняя панель со ссылками.
-    //Ссылки вида <a href="#contacts">Контакты</a>
-    $(".top_mnu").navigation();
-
-    //Добавляет классы дочерним блокам .block для анимации
-    //Документация: http://imakewebthings.com/jquery-waypoints/
-    $(".block").waypoint(function(direction) {
-        if (direction === "down") {
-            $(".class").addClass("active");
-        } else if (direction === "up") {
-            $(".class").removeClass("deactive");
-        };
-    }, {offset: 100});
-
-    //Плавный скролл до блока .div по клику на .scroll
-    //Документация: https://github.com/flesler/jquery.scrollTo
-    $("a.scroll").click(function() {
-        $.scrollTo($(".div"), 800, {
-            offset: -90
-        });
-    });
-
-    //Каруселька
-    //Документация: http://owlgraphic.com/owlcarousel/
-    var owl = $(".carousel");
-    owl.owlCarousel({
-        items : 4
-    });
-    owl.on("mousewheel", ".owl-wrapper", function (e) {
-        if (e.deltaY > 0) {
-            owl.trigger("owl.prev");
-        } else {
-            owl.trigger("owl.next");
-        }
-        e.preventDefault();
-    });
-    $(".next_button").click(function(){
-        owl.trigger("owl.next");
-    });
-    $(".prev_button").click(function(){
-        owl.trigger("owl.prev");
-    });
-
     //Кнопка "Наверх"
     //Документация:
     //http://api.jquery.com/scrolltop/
@@ -66,23 +10,6 @@ $(document).ready(function() {
         }, 800);
         return false;
     });
-    
-    //Аякс отправка форм
-    //Документация: http://api.jquery.com/jquery.ajax/
-    $("form").submit(function() {
-        $.ajax({
-            type: "GET",
-            url: "mail.php",
-            data: $("form").serialize()
-        }).done(function() {
-            alert("Спасибо за заявку!");
-            setTimeout(function() {
-                $.fancybox.close();
-            }, 1000);
-        });
-        return false;
-    });
-
 
     // Выбор дня
     $(".day_select").click(function() {
@@ -123,34 +50,30 @@ $(document).ready(function() {
             }
         }
     }
+    
     //Popup
-
     function getPosition(e) {
-      var posx = 0;
-      var posy = 0;
+        var posx = 0;
+        var posy = 0;
 
-      if (!e) var e = window.event;
+        if (!e) var e = window.event;
 
-      if (e.pageX || e.pageY) {
+        if (e.pageX || e.pageY) {
         posx = e.pageX;
         posy = e.pageY;
-      }
-      else if (e.clientX || e.clientY) {
+        }
+        else if (e.clientX || e.clientY) {
         posx = e.clientX + document.body.scrollLeft
           + document.documentElement.scrollLeft;
         posy = e.clientY + document.body.scrollTop
           + document.documentElement.scrollTop;
-      }
+        }
 
-      return {
+        return {
         x: posx,
         y: posy
-      }
+        };
     }
-
-    //$(window).mousemove(function (pos) {
-    //    $(".pop_up").css('left',(pos.pageX+25)+'px').css('top',(pos.pageY-5)+'px');
-    //});
 
     $(window).mousemove(function (pos) {
         var elem_height = $(".pop_up").outerHeight();
@@ -176,9 +99,6 @@ $(document).ready(function() {
             $(".pop_up").css('top',posy - elem_height / 2 +'px');
         }
 
-        //if (posy + elem_height / 2 + margin_bottom > window_height * Math.floor((posy + elem_height / 2 + margin_bottom) / window_height)) {
-        //    $(".pop_up").css('top',window_height * Math.floor((posy + elem_height / 2 + margin_bottom) / window_height) - elem_height - margin_bottom + 'px');
-        //};
     });
 
     $('.tv_show').hover(
@@ -186,7 +106,6 @@ $(document).ready(function() {
             $(".pop_up").css('display','block');
         },
         function() {
-            $(".pop_up").css('display','none');
-            
+            $(".pop_up").css('display','none');  
         });
 });
